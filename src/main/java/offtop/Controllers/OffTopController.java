c3f465327c20b0c9deb9c84cefbdea54cf2f0d68
asbin4747 <asbin.dahal4747@gmail.com>
2019-11-21 10:27:13 -0800

    Initializing Spring Boot Repository

---
+++ src/main/java/offtop/Controllers/OffTopController.java
@@ -0,0 +1,14 @@
+package offtop.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OffTopController{
+
+
+    @GetMapping("/Rest")
+    public String Test(){
+        return "Api works";
+    }
+}
---
+++ src/main/java/offtop/OffTopApplication.java
@@ -0,0 +1,13 @@
+package offtop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffTopApplication.class, args);
+	}
+
+}
