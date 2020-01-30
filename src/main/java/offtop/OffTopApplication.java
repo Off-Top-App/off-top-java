a83ce5404df6b03bb233cf8ea47fbb6c79eb4b7b
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-01-29 17:05:40 -0800

    configured java project to work with Apache Spark.

--- src/main/java/offtop/OffTopApplication.java
+++ src/main/java/offtop/OffTopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OffTopApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(OffTopApplication.class, args);
 	}
 
