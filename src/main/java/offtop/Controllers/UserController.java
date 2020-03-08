6cefda32d297751de617da5fa1c1150a57071612
asbin4747 <asbin.dahal4747@gmail.com>
2020-03-07 17:44:00 -0800

    OT-72 Created a post request to fill in MYSQL database

--- src/main/java/offtop/Controllers/UserController.java
+++ src/main/java/offtop/Controllers/UserController.java
@@ -1,12 +1,10 @@
 package offtop.Controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import offtop.Config.UserRepository;
+import offtop.Repository.UserRepository;
 import offtop.Models.User;
 
 @RestController
@@ -15,10 +13,8 @@ public class UserController{
     @Autowired
     UserRepository database;
     
-    @PostMapping(value ="/offTop",consumes = "application/json")
+    @PostMapping(value ="/setUser")
     public User setUser (@RequestBody User user){
          return database.save(user);
     }
 }
-//1, "firstName", "lastName", "city", 20, "gender", "professional",
-//"email", "username", "password", "createdAt", "deletedAt"
--- src/main/java/offtop/Repository/UserRepository.java
+++ src/main/java/offtop/Repository/UserRepository.java
@@ -1,12 +1,11 @@
-package offtop.Config;
+package offtop.Repository;
 
 import offtop.Models.User;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called DatabaseRepo
+// This will be AUTO IMPLEMENTED by Spring into a Bean called UserRepository
 // CRUD refers Create, Read, Update, Delete
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
