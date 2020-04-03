ca250237e941154813eda8c91c5e04de7622c73c
Zane Ervin <zaneerv@gmail.com>
2020-04-03 08:00:14 -0700

    created the controller to recieve the user data

---
+++ src/main/java/offtop/Controllers/UserController.java
@@ -0,0 +1,31 @@
+package offtop.Controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import offtop.Repository.UserRepository;
+import offtop.Models.User;
+
+@RestController
+public class UserController{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping(value ="/setUser")
+    public User setUser (@RequestBody User user){
+        System.out.println("***Post Request Created Successfully***");
+         return userRepository.save(user);
+    }
+    
+    @GetMapping(value = "/user/{email}")
+    public User getUser( @PathVariable("email")String email) {
+        User user = userRepository.findByEmail(email).get(0);
+        return user;
+        
+    }
+}
