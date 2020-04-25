be7d2ac05569d55013d03d9c01ebb359c1525c2c
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-04-24 21:19:39 -0700

    working on converting bytes to aac files

--- src/main/java/offtop/Services/WebsocketService.java
+++ src/main/java/offtop/Services/WebsocketService.java
@@ -30,48 +30,10 @@ public class WebsocketService {
   private final Producer producer;
 
   public WebsocketService(Producer producer) {
-    this.producer = producer;
+	this.producer = producer;
   }
-  private static byte[] addWavHeader(byte[] bytes) throws IOException {
 
-		ByteBuffer bufferWithHeader = ByteBuffer.allocate(bytes.length + 44);
-		bufferWithHeader.order(ByteOrder.LITTLE_ENDIAN);
-		bufferWithHeader.put("RIFF".getBytes());
-		bufferWithHeader.putInt(bytes.length + 36);
-		bufferWithHeader.put("WAVE".getBytes());
-		bufferWithHeader.put("fmt ".getBytes());
-		bufferWithHeader.putInt(16);
-		bufferWithHeader.putShort((short) 1);
-		bufferWithHeader.putShort((short) 1);
-		bufferWithHeader.putInt(16000);
-		bufferWithHeader.putInt(32000);
-		bufferWithHeader.putShort((short) 2);
-		bufferWithHeader.putShort((short) 16);
-		bufferWithHeader.put("data".getBytes());
-		bufferWithHeader.putInt(bytes.length);
-		bufferWithHeader.put(bytes);
-		return bufferWithHeader.array();
-	}
-
-  public static AudioInputStream getAudioStream(byte[] byteArray) {
-		try {
-			try {
-				ByteArrayInputStream byteStream =
-						new ByteArrayInputStream(byteArray);
-				return AudioSystem.getAudioInputStream(byteStream);
-			}
-			catch (UnsupportedAudioFileException e) {
-				byteArray = addWavHeader(byteArray);
-				ByteArrayInputStream byteStream =
-						new ByteArrayInputStream(byteArray);
-				return AudioSystem.getAudioInputStream(byteStream);
-			}
-		}
-		catch (IOException | UnsupportedAudioFileException e) {
-			throw new RuntimeException("cannot convert bytes to audio stream: " + e);
-		}
-	}
-  public void writeToAudioFile(ArrayList<Double> audioData) {
+	public void writeToAudioFile(ArrayList<Double> audioData) {
     byte[] byteArray = new byte[1024];
 
     Iterator<Double> iterator = audioData.iterator();
@@ -79,41 +41,68 @@ public class WebsocketService {
     System.out.println(byteArray);
 
     while (iterator.hasNext()) {
-      Integer i = iterator.next().intValue();
+	  Integer i = iterator.next().intValue();
       byteArray[i] = i.byteValue();
     }
     // NOTE: the following try-catch will attempt to create the audio file
-    // try {
-    //   File someFile = new File("AudioFile.mp3");
-    //   FileOutputStream fos = new FileOutputStream(someFile);
-    //   fos.write(byteArray);
-    //   fos.flush();
-    //   fos.close();
-
-    //   System.out.println("File created");
-    // } catch (Exception e) {
-    //   // TODO: handle exception
-    //   System.out.println("Error: " + e);
-    // }
-
     try {
-      File someFile = new File("test.wav");
-			AudioInputStream audioStream = getAudioStream(byteArray);
-			if (someFile.getName().endsWith("wav")) {
-				int nb = AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE,
-            new FileOutputStream(someFile));
-        System.out.println("file written");
-				// log.fine("WAV file written to " + someFile.getCanonicalPath()
-				// 		+ " (" + (nb / 1000) + " kB)");
-			}
-			else {
-				throw new RuntimeException("Unsupported encoding " + someFile);
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("could not generate file: " + e);
-		}
+			// File outputFile = File.createTempFile("MediaFile", "aac", getCacheDir());
+      File someFile = new File("AudioFile.aac");
+      FileOutputStream fos = new FileOutputStream(someFile);
+      fos.write(byteArray);
+      fos.flush();
+      fos.close();
+
+      System.out.println("File created");
+    } catch (Exception e) {
+      // TODO: handle exception
+      System.out.println("Error: " + e);
+    }
+
+
+	//Note that input file type does not depend
+    // on file name or extension.
+    // File inputFileObj = new File(args[0]);
+    // AudioInputStream audioInputStream = null;
+    // try{
+    //   audioInputStream = AudioSystem.
+    //            getAudioInputStream(inputFileObj);
+    // }catch (Exception e){
+    //   e.printStackTrace();
+    //   System.exit(0);
+    // }//end catch
+
+    // System.out.println("Input file format:");
+    // showFileType(inputFileObj);
+
+    // int bytesWritten = 0;
+    // try{
+    //   bytesWritten = AudioSystem.
+    //                     write(audioInputStream,
+    //                           outputType,
+    //                           new File(args[1]));
+    // }catch (Exception e){
+    //   e.printStackTrace();
+	//   System.exit(0)
+	  
+    // try {
+    //   File someFile = new File("test.wav");
+	// 	AudioInputStream audioStream = AudioSystem.getAudioInputStream(someFile);
+	// 	if (someFile.getName().endsWith("wav")) {
+	// 		int nb = AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE,
+	// 	new FileOutputStream(someFile));
+	// System.out.println("file written");
+	// 		// log.fine("WAV file written to " + someFile.getCanonicalPath()
+	// 		// 		+ " (" + (nb / 1000) + " kB)");
+	// 	}
+	// 	else {
+	// 		throw new RuntimeException("Unsupported encoding " + someFile);
+	// 	}
+	// }
+	// catch (Exception e) {
+	// 	e.printStackTrace();
+	// 	throw new RuntimeException("could not generate file: " + e);
+	// }
   }
 
   public void sendFileEvent(AudioEvent audioEvent) {
