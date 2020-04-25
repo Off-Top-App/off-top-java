3c3df36061102091d1bc24425226ba99b2f4749d
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-04-25 01:44:52 -0700

    included the service that writes binary data to audio files

--- src/main/java/offtop/Services/AudioService.java
+++ src/main/java/offtop/Services/AudioService.java
@@ -1,5 +1,7 @@
 package offtop.Services;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,26 @@ public class AudioService{
     @Autowired
     Producer producer;
 
+    //only being used when wanting to write files
+    public void writeBytesToFile(List<Double> audioData){
+        try {
+            File someFile = new File("AudioFile.wav");
+            FileOutputStream fos = new FileOutputStream(someFile);
+            fos.write(writeToBytes(audioData));
+            fos.flush();
+            fos.close();
+            System.out.println("File created");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+    byte[] writeToBytes(List<Double> audioData) {
+        byte[] result = new byte[audioData.size()];
+        for (int i = 0; i < audioData.size(); i++) {
+            result[i] = audioData.get(i).byteValue();
+        }
+        return result;  
+    }
     public void converToByteDataAndProduceMessage(ArrayList<Double> audioData, AudioEvent originalAudioEvent ) {
         byte[] audioBytes = writeToBytes(audioData);
         AudioEvent audioEvent = new AudioEvent(
@@ -23,15 +45,8 @@ public class AudioService{
           originalAudioEvent.getTopic()
         );
         sendAudioEvent(audioEvent);
-      }
-
-    byte[] writeToBytes(List<Double> audioData) {
-        byte[] result = new byte[audioData.size()];
-        for (int i = 0; i < audioData.size(); i++) {
-          result[i] = audioData.get(i).byteValue();
-        }
-        return result;  
     }
+
 
     public void sendAudioEvent(AudioEvent audioEvent) {
         producer.sendAudioFile(audioEvent);
