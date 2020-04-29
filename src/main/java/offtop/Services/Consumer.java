f88ccfd00dd4cd04c025c6c74a307ed1122df7ee
Zane Ervin <zaneerv@gmail.com>
2020-04-28 23:45:24 -0700

    parsed the string correctly and sends the data to websockets

--- src/main/java/offtop/Services/Consumer.java
+++ src/main/java/offtop/Services/Consumer.java
@@ -9,22 +9,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import offtop.Config.WebsocketHandler;
+
 import java.util.Map;
 
 @Service
 public class Consumer {
     private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+    private WebsocketHandler handler;
+
+    public Consumer(WebsocketHandler handler) {
+        this.handler = handler;
+    }
 
     // Receives message from the producer and displays into console
     // KafkaListner suscribes to a topic and receives all the message sent into that
     // topic
     @KafkaListener(topics = "outgoingFocusScore", groupId = "group_Id")
-    public void receive(String message) throws IOException {
-
-        Map value = new Gson().fromJson(message, Map.class);
-
+    public void receiveFocusScore(String message) throws IOException {
+        message = message.substring(1, message.length() - 1);
+        String _message = message.replaceAll("\\\\\"", "\"");
+        System.out.println("_message: " + _message);
+        Map value = new Gson().fromJson(_message, Map.class);
+        logger.info(String.format("The message you entered -> %s", value.toString()));
+        double userId = (double) value.get("user_id");
+        handler.sendConsumerData(userId, message);
         logger.info(String.format("The message you entered -> %s", value));
     }
-
 
 }
